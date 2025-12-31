fe9a188692286c11bfcf8ac6946026a04fc95e93
Mahamure Kedar <kedarmahamura12@gmail.com>
2025-12-31 11:47:29 +0530

    deleted unwanted images and updated the favicon

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-         <link rel="icon" type="image/png" sizes="256x256" href="/favicon-512x512.png"/>
+         <link rel="icon" type="image/png" sizes="256x256" href="/mk.png"/>
       </head>
       <body className={`${inter.className}`} suppressHydrationWarning>
          <ThemeProvider
