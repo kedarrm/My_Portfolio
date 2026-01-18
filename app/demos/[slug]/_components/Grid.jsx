8c297ee4def7b01ea1d3a40d30b603f74c31bce3
Mahamure Kedar <kedarmahamura12@gmail.com>
2026-01-18 15:44:58 +0530

    minor style  fixes

--- app/demos/[slug]/_components/Grid.jsx
+++ app/demos/[slug]/_components/Grid.jsx
@@ -9,13 +9,13 @@ const GridLayout = () => {
       className="
         relative h-72 w-full overflow-auto
         rounded-lg 
-        m-4 mask-b-from-1
+        m-3 mask-b-from-1
       "
       initial={{ opacity: 0, filter: "blur(24px)" }}
       animate={{ opacity: 1, filter: "blur(0px)" }}
       transition={{ duration: 0.8, ease: "easeOut" }}
     >
-      <div className="columns-3 gap-2 px-2 py-3">
+      <div className="columns-3 gap-1.5 px-1.5 py-2">
         {[...Array(12)].map((_, i) => (
           <div
             key={i}
